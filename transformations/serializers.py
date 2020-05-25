7e9b219755b814f2f4ff0a3d49a14efec6bd8f13
KonradMarzec1991 <konrimarzec@gmail.com>
2020-05-25 21:36:54 +0200

    Add descriptor to Employee model

--- transformations/serializers.py
+++ transformations/serializers.py
@@ -2,13 +2,16 @@ from rest_framework import serializers
 
 
 class EmployeeSerializer(serializers.Serializer):
+
+
+
     FirstName = serializers.CharField(max_length=100)
     LastName = serializers.CharField(max_length=100)
     Address = serializers.CharField(max_length=100)
     MaritalStatus = serializers.CharField(max_length=100)
     Gender = serializers.CharField(max_length=100)
     Salary = serializers.CharField(max_length=100)
-    Age = serializers.CharField(max_length=100)
+    Age = serializers.IntegerField()
     Interests = serializers.CharField(max_length=100)
     DateOfJoining = serializers.CharField(max_length=100)
     Designation = serializers.CharField(max_length=100)
