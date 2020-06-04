f7a30833b269aa17522e7e0288383733f1e848d0
KonradMarzec1991 <konrimarzec@gmail.com>
2020-06-04 22:47:02 +0200

    Add viewset client test

--- transformations/tests/test_views.py
+++ transformations/tests/test_views.py
@@ -1,12 +1,34 @@
 from rest_framework.test import APIClient
-from transformations.views import GeneralFilterViewSet
 from .conftest import Error
 
 
 class TestGeneralFilteViewSet:
 
-    def test_view_args(self):
+    def test_view_gender(self):
         client = APIClient()
         response = client.get('/general/?gender=XYZT')
         assert response.status_code == 404
         assert response.data['gender'][0] == Error.GENDER_ERROR.value
+
+        response = client.get('/general/?gender=Male')
+        assert response.status_code == 200
+
+    def test_view_martial_status(self):
+        client = APIClient()
+        response = client.get('/general/?martial_status=abcdef')
+        assert response.status_code == 404
+        assert response.data['martial_status'][0] == Error.MARTIAL_ERROR.value
+
+        response = client.get('/general/?martial_status=Married')
+        assert response.status_code == 200
+
+    def test_view_age(self):
+        client = APIClient()
+        response = client.get('/general/?age=-50')
+        assert response.status_code == 404
+        assert response.data['age'][0] == Error.AGE_ERROR.value
+
+        response = client.get('/general/?age=35')
+        assert response.status_code == 200
+
+
