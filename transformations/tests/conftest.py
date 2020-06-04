afc1b70e13de5d0782db76a2f47cbf3e93a8813b
KonradMarzec1991 <konrimarzec@gmail.com>
2020-06-04 22:41:18 +0200

    Move Enum Error to conftest

--- transformations/tests/conftest.py
+++ transformations/tests/conftest.py
@@ -1,6 +1,12 @@
 import pytest
-
+from enum import Enum
 from transformations.models import Employee
+
+
+class Error(Enum):
+    AGE_ERROR = 'Age must greater than 0'
+    MARTIAL_ERROR = 'Unmarried or Married allowed'
+    GENDER_ERROR = 'Gender must be female or male'
 
 
 @pytest.fixture
