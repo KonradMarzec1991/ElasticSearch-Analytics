0ff39fd5491e71be0a98b1723990d7919c5dc245
KonradMarzec1991 <konrimarzec@gmail.com>
2020-07-11 20:02:46 +0200

    Add new action url to FilterViewset

--- transformations/views.py
+++ transformations/views.py
@@ -1,6 +1,25 @@
+"""
+ElasticSearch Viewsets
+"""
+
+from django.http import HttpResponse
+
+try:
+    from io import BytesIO as IO
+except ImportError:
+    from io import StringIO as IO
+
+import pandas as pd
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from .serializers import FirstNameSerializer, GeneralFilterSerializer
+
+from es_core.es_filters import filter_by_age
+from .es_to_excel import normalize_es_output
+
+
+EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 
 
 def short_view(class_serializer):
@@ -17,6 +36,23 @@ def short_view(class_serializer):
 class FilterByNameViewSet(viewsets.ViewSet):
     """Viewset filters by first_name"""
     list = short_view(FirstNameSerializer)
+
+    @action(detail=False)
+    def export_to_excel(self, request):
+        es_retrieve = filter_by_age(50)
+        df = normalize_es_output(es_retrieve)
+
+        io_file = IO()
+        excel_file = pd.ExcelWriter(io_file, engine='xlsxwriter')
+        df.to_excel(excel_file, sheet_name='es_stats')
+
+        excel_file.save()
+        excel_file.close()
+        io_file.seek(0)
+
+        response = HttpResponse(excel_file.read(), content_type=EXCEL_TYPE)
+        response['Content-Disposition'] = 'attachment; filename=stats.xlsx'
+        return response
 
 
 class GeneralFilterViewSet(viewsets.ViewSet):
