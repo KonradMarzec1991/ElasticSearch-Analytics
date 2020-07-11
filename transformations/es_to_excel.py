89d798e8c97a2a914ae6cf1bcb6af245c98442c1
KonradMarzec1991 <konrimarzec@gmail.com>
2020-07-11 19:41:03 +0200

    Normalize pandas dataFrame

--- transformations/es_to_excel.py
+++ transformations/es_to_excel.py
@@ -1,9 +1,10 @@
 """
 Pandas setup and helper functions
 """
-
 import pandas as pd
 from pandas import json_normalize
+
+columns_to_drop = ['_index', '_type', '_id', '_score']
 
 
 def drop_prefix(self, prefix):
@@ -21,4 +22,25 @@ def drop_suffix(self, suffix):
 # Add functions to pd
 pd.core.frame.DataFrame.drop_prefix = drop_prefix
 pd.core.frame.DataFrame.drop_suffix = drop_suffix
+
+
+def normalize_es_output(es_output):
+    df = json_normalize(es_output)
+    df.drop(columns_to_drop, inplace=True, axis='columns')
+    df.drop_suffix('_source.')
+
+    df['Salary'] = df['Salary'].astype(int)
+    df['DateOfJoining'] = pd.to_datetime(df['DateOfJoining'])
+
+    df['FullName'] = df[['FirstName', 'LastName']]\
+        .agg(' '.join, axis='columns').str.title()
+    df.drop(['FirstName', 'LastName', "Address"], inplace=True, axis='columns')
+
+    cols = df.columns.to_list()
+    cols = cols[-2:] + cols[:-2]
+    df = df[cols]
+
+    df.sort_values('Salary', inplace=True)
+    return df
+
 
